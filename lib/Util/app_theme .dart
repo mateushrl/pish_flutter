2b88fae5bd1abafdc7757769d71c35bece95cf22
Mateus Ramalho <mateushrl918@gmail.com>
2023-11-02 11:36:37 -0300

    criacao do layout da tela de login e login autenticado com o firebase

---
+++ lib/Util/app_theme .dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF4CAF50);
+  static const Color secondaryColor = Color(0xFF388E3C); 
+  static const Color accentColor = Color(0xFF689F38); 
+
+  static const TextStyle titleTextStyle = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle subtitleTextStyle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle bodyTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pish_flutter/pages/home_page.dart';
+
+import '../Util/app_theme .dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,8 +12,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController senhaController = TextEditingController();
+  TextEditingController emailController =
+      TextEditingController(text: "mateushrl918@gmail.com");
+  TextEditingController senhaController =
+      TextEditingController(text: '123456789');
+  final _formKey = GlobalKey<FormState>();
+  bool _obscureText = true;
 
   @override
   void initState() {
@@ -24,91 +27,164 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        title: const Text(''),
-      ),
-      body: Center(
-        child: Card(
-          elevation: 3,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Container(
-            height: MediaQuery.of(context).size.width / 3.5,
-            width: MediaQuery.of(context).size.width / 1.5,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
-            child: Row(
-              children: [
-                Flexible(
-                  child: Container(
-                    padding: EdgeInsets.all(100),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Text(
-                          "Login",
-                          style: TextStyle(color: Colors.black, fontSize: 50),
-                        ),
-                        //Image.asset('assets/logo.png'),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        Form(
-                          child: Column(
-                            children: [
-                              TextField(
-                                controller: emailController,
-                                keyboardType: TextInputType.emailAddress,
-                                decoration: const InputDecoration(
-                                  labelText: 'E-mail',
-                                ),
-                              ),
-                              TextField(
-                                keyboardType: TextInputType.text,
-                                controller: senhaController,
-                                obscureText: true,
-                                decoration: const InputDecoration(
-                                  labelText: 'Senha',
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => const HomePage(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            width: double.infinity,
-                            alignment: Alignment.center,
-                            height: 30,
-                            decoration: BoxDecoration(
-                                color: Colors.yellow,
-                                borderRadius: BorderRadius.circular(2)),
-                            child: Text("Entrar"),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+        body: Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('fundoLogin.jpg'),
+              fit: BoxFit.cover,
             ),
           ),
         ),
-      ),
-    );
+        Center(
+          child: Container(
+            width: 450,
+            height: 500,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromARGB(255, 255, 255, 255),
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: EdgeInsets.fromLTRB(20, 20, 20, 60),
+                      child: const Text(
+                        "Login",
+                        style: AppTheme.titleTextStyle,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        prefixIcon: Icon(
+                          Icons.email,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppTheme.primaryColor, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Informe um e-mail válido.";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: TextFormField(
+                      obscureText: _obscureText,
+                      controller: senhaController,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        border: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        prefixIcon: const Icon(
+                          Icons.password,
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          child: Icon(
+                            _obscureText
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppTheme.primaryColor, width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Informe uma senha válida.";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    width: double.infinity,
+                    height: 85,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processando...')),
+                          );
+                          bool autorizado = await login();
+                          nextPage(autorizado);
+                        }
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(AppTheme.primaryColor),
+                      ),
+                      child: const Text("Entrar",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Future<bool> login() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: senhaController.text);
+      if (credential.user?.uid != null) {
+        return true;
+      }
+      return false;
+    } on FirebaseAuthException catch (e) {
+      return false;
+    }
+  }
+
+  void nextPage(bool autorizado) {
+    if (autorizado) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content:
+                Text('Acesso não autorizado, verifique suas credenciais.')),
+      );
+    }
   }
 }
